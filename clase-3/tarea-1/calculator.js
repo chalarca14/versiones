ad9c5c42a2991fbda0dd930ef59305d8f208279e
santiago <santiagoschalarca@gmail.com>
2025-11-07 11:37:39 -0500

    feat:(calculator.js) se añadio una funcion de suma, porque el cliente pidio esta nueva funcion

---
+++ clase-3/tarea-1/calculator.js
@@ -0,0 +1,8 @@
+let num1 =parseInt (prompt("ingresa el primer numero a sumar : "));
+let num2 =parseInt (prompt("ingresa el segundo numero a sumar : "));
+
+
+let res = parseInt(num1) + parseInt(num2);
+
+
+console.log(`la suma de ${num1} + ${num2} es : ${res}`);
